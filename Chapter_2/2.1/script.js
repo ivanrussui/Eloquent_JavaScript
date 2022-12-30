ee20e38f56e39354dd0c9437fa8ef67f5cf32957
Ivan <fenomen_ny@mail.ru>
2022-12-30 09:28:10 +0300

    feat 2 задание сделано более элегантно, убрал комментарии со всех задач

--- Chapter_2/2.1/script.js
+++ Chapter_2/2.1/script.js
@@ -3,5 +3,5 @@ const n = '#######';
 const nLength = n.length;
 
 for (let i = '#'; i.length <= nLength; i += '#') {
-	console.log(i);
+    console.log(i);
 }
--- Chapter_2/2.2/script.js
+++ Chapter_2/2.2/script.js
@@ -1,28 +1,17 @@
-// сначала сделал так тут отличие в том, что от 0
-// let n = 100;
-//
-// for (let i = 0; i <= n; i++) {
-//     if (i % 3 === 0 && i % 5 === 0 && i !== 0) {
-//         console.log('FizzBuzz');
-//     } else if (i % 3 === 0 && i !== 0)  {
-//         console.log('Fizz');
-//     } else if (i % 5 === 0 && i !== 0) {
-//         console.log('Buzz');
-//     } else {
-//         console.log(i);
-//     }
-// }
-
-
 const n = 100;
 
 for (let i = 1; i <= n; i++) {
-    if (i % 3 === 0 && i % 5 === 0) {
-        console.log('FizzBuzz');
-    } else if (i % 3 === 0)  {
-        console.log('Fizz');
-    } else if (i % 5 === 0) {
-        console.log('Buzz');
+    let res = '';
+
+    if (i % 3 === 0) {
+        res = 'Fizz';
+    }
+    if (i % 5 === 0) {
+        res += 'Buzz';
+    }
+
+    if (res) {
+        console.log(res);
     } else {
         console.log(i);
     }
