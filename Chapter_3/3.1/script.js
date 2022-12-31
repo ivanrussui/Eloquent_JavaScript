709b8730ad50e63868bc9287829d30875a039784
Ivan <fenomen_ny@mail.ru>
2022-12-31 13:46:10 +0300

    feat написал несколько видов реализации функции минимум

---
+++ Chapter_3/3.1/script.js
@@ -0,0 +1,47 @@
+// function min(a, b) {
+//     if (a < b) {
+//         return a;
+//     }
+//     if (b < a) {
+//         return b;
+//     }
+// }
+//
+// console.log(min(13, 22));
+
+
+// function min(a, b) {
+//     if (a < b) {
+//         return a;
+//     } else {
+//         return b;
+//     }
+// }
+//
+// console.log(min(13, 22));
+
+
+// function min(a, b) {
+//     const res = a < b;
+//     console.log(res ? a : b)
+// }
+//
+// min(3, 22);
+
+
+// const min = function(a, b) {
+//     return a < b ? a : b
+// }
+//
+// console.log(min(33, 22));
+
+
+// const min = (a, b) => {
+//     console.log(a < b ? a : b)
+// }
+// min(3, 22)
+
+
+const min = (a, b) => a < b ? a : b;
+
+console.log(min(33, 22));
